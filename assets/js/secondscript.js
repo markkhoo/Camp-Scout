a70b7d3a5aad092a99ce0d81021ed9556ca90fa7
javimarashall <jamondragon@ucdavis.edu>
2021-04-07 10:52:34 -0700

    fetches work

--- assets/js/secondscript.js
+++ assets/js/secondscript.js
@@ -1,5 +1,26 @@
 
-fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=34.0522&lon=-118.2437&page=1&per_page=6&radius=50", {
+var city = "san francisco";
+var latitude;
+var longitude;
+var APIKey = "b7d72c9e1d3c51bb5697223f11eda7dd"
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+
+fetch (queryURL)
+.then(function (response) {
+    console.log("response", response);
+    return response.json();
+})
+.then(function (data){
+    console.log("data", data);
+	latitude = data.city.coord.lat;
+	longitude = data.city.coord.lon;
+	console.log(latitude);
+	console.log(longitude);
+
+
+
+
+fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat="+ latitude+ "&lon=" + longitude + "&page=1&per_page=6&radius=50", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "6cbc683214mshdf5038bf7d4e267p15a6f0jsn91fc8c373911",
@@ -13,24 +34,19 @@ fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=34.0522&lon=
 .then(function (data){
     console.log("data", data);
 })
-
-
-
-
-
-var latitude = 34.0522;
-var longitude= -118.2437;
-var APIKey = "b7d72c9e1d3c51bb5697223f11eda7dd"
-var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude+ "&appid=" + APIKey;
-
-fetch (queryURL)
-.then(function (response) {
-    console.log("response", response);
-    return response.json();
 })
-.then(function (data){
-    console.log("data", data);
-})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // var APIKey = "27ffe38d-a349-4d2d-ae63-4d825afba66c";
