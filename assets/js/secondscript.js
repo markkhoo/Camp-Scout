543462cb755e2d5deb4c73afc7bbc87ddeaedbf8
javimarashall <jamondragon@ucdavis.edu>
2021-04-06 22:54:28 -0700

    trail api

--- assets/js/secondscript.js
+++ assets/js/secondscript.js
@@ -1,7 +1,41 @@
-//fetch recreation API
+
+fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=34.0522&lon=-118.2437&page=1&per_page=6&radius=50", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "6cbc683214mshdf5038bf7d4e267p15a6f0jsn91fc8c373911",
+		"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+    return response.json();
+})
+.then(function (data){
+    console.log("data", data);
+})
+
+
+
+
+
+var latitude = 34.0522;
+var longitude= -118.2437;
+var APIKey = "b7d72c9e1d3c51bb5697223f11eda7dd"
+var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude+ "&appid=" + APIKey;
+
+fetch (queryURL)
+.then(function (response) {
+    console.log("response", response);
+    return response.json();
+})
+.then(function (data){
+    console.log("data", data);
+})
+
 
 // var APIKey = "27ffe38d-a349-4d2d-ae63-4d825afba66c";
-// var queryURL = "http://ridb.recreation.gov/api/v1/campsites?limit=6&offset=0&apikey=27ffe38d-a349-4d2d-ae63-4d825afba66c";
+// var queryURL = 
+// //"http://ridb.recreation.gov/api/v1/campsites?limit=6&offset=0&apikey=27ffe38d-a349-4d2d-ae63-4d825afba66c";
 
 // fetch(queryURL, {
 //     mode: 'no-cors',
@@ -10,17 +44,6 @@
 //     //convert the response into JSON 
 //     .then(function (response) {
 //       console.log("response", response)
-//       return response.json();
+//       return response;
 //     })
-//     .then(function (data) {
-//         console.log("data", data)
-//     })
-
-var APIKey = "b7d72c9e1d3c51bb5697223f11eda7dd"
-var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude+ "&exclude={part}&appid=" + APIKey;
-var latitude;
-var longitude;
-fetch (queryURL)
-.then(function (response) {
-    console.log("response", response)
-})
+   
