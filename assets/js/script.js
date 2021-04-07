377354bbaad9410a32ff9e47c2012cd9b01cc8e4
markeezy <markkhoo95@gmail.com>
2021-04-07 13:14:30 -0700

    add search function for trails

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,101 @@
-
-// ======================================================================================================
-// === TESTING CODE =====================================================================================
-// ======================================================================================================
-
-// Fetch Information
-var keyRec = "bf9a0395-5e31-4e13-a9d3-7192327f0be0";
-// RIDB API
-var urlTest1 = "https://ridb.recreation.gov/api/v1/facilities?offset=0&state=CO&latitude=-118.0186111&longitude=43.88037021&radius=9.75&activity=6,BOATING&lastupdated=10-01-2018&apikey=" + keyRec;
-// OpenAQ
-var urlTest2 = "https://docs.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&coordinates=38.3%2C-122.2&radius=1000&order_by=lastUpdated&dumpRaw=false";
-
-function urlFecth (url) {
-    fetch(url, {
-        mode: "no-cors",
-        headers: { 'Content-Type': 'text/plain' }
-    })
-    .then(function(response) {
-        console.log(response);
-        console.log(response.status);
-    
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    })
-    .catch( err => {
-        console.log(err);
-    });
-};
-
-urlFecth(urlTest1);
-urlFecth(urlTest2);
-
-
-// An API fetch that actually works
-// fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=34.0522&lon=-118.2437&page=1&per_page=6&radius=50", {
-//     "method": "GET",
-//     "headers": {
-//         "x-rapidapi-key": "6cbc683214mshdf5038bf7d4e267p15a6f0jsn91fc8c373911",
-//         "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
-//     }
-// })
-// .then(response => {
-//     console.log(response);
-//     return response.json();
-// })
-// .then(function (data){
-//     console.log("data", data);
-// })
-
-
-
 // ======================================================================================================
 // ==========================================   Actual  Code   ==========================================
 // ======================================================================================================
+
+// Global Variables
+var searchLat = 0;
+var searchLon = 0;
+var currentPageNum = 0;
+var maximumPageNum = 0;
+
+// Trail Search Function
+function trailSearch(pageNum, lat, lon) {
+    var results_Number = 12;
+    fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat="+ lat + "&lon=" + lon + "&page=" + pageNum + "&per_page=" + results_Number + "&radius=100", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "6cbc683214mshdf5038bf7d4e267p15a6f0jsn91fc8c373911",
+            "x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com"
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(function (data){
+        maximumPageNum = Math.ceil(data.results / results_Number);
+        console.log(data);
+    });
+};
+
+// Form Submission
+var searchForm = document.getElementById("location");
+
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var cityName = searchForm.firstElementChild.value.toLowerCase();
+    var keyWeather = "22bb6e2db366aab8539ac22df7b32d3a"
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + keyWeather;
+
+    fetch(weatherURL)
+    .then(function(response) {
+        if (response.status == 200) {
+            return response.json();
+        } else {
+            return null;
+        };
+    })
+    .then(function(data) {
+        console.log(data);
+        console.log(data.coord.lat);
+        console.log(data.coord.lon);
+
+        searchLat = data.coord.lat;
+        searchLon = data.coord.lon;
+        currentPageNum = 1;
+
+        trailSearch(currentPageNum,searchLat,searchLon);
+
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+});
+
+// Next Button
+document.getElementById('nextBtn').addEventListener('click', function(){
+    if (currentPageNum < maximumPageNum) {
+        currentPageNum++;
+        trailSearch(currentPageNum,searchLat,searchLon);
+    };
+
+    console.log('next');
+});
+
+// Previous Button
+document.getElementById('prevBtn').addEventListener('click', function(){
+    if (currentPageNum > 1) {
+        currentPageNum--;
+        trailSearch(currentPageNum,searchLat,searchLon);
+    };
+
+    console.log('prev');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // var currentTemp = 0;
 // var currentHumi = 0;
